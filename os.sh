0a2fd4595b88cf5bbffeafd85cc8142be962c754
Keith Pitt <me@keithpitt.com>
2025-05-24 23:33:02 +0800

    Added a new devfile to setup the OS env

---
+++ os.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env devmachine
+
+case "$1" in
+
+  shellenv)
+    echo '
+    export XDG_CONFIG_HOME="$HOME/.config"
+    export XDG_CACHE_HOME="$HOME/.cache"
+    export XDG_DATA_HOME="$HOME/.local/share"
+    export XDG_STATE_HOME="$HOME/.local/state"
+    export XDG_RUNTIME_DIR="/run/user/$UID"
+    export XDG_BIN_HOME="$HOME/.local/bin"
+    export PATH="$XDG_BIN_HOME:$PATH"
+    '
+    ;;
+
+  --check-priority)
+    echo 1000
+    ;;
+
+esac
