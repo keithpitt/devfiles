a67b56a388e45096d8a8133e87a3d51c4f2e4228
Keith Pitt <git@keithpitt.email>
2025-05-27 13:42:31 +0800

    Don't show rando chars in ghostty

--- zsh.sh
+++ zsh.sh
@@ -16,6 +16,7 @@ case "$1" in
   shellenv)
     if [[ "$2" == "zsh" ]]; then
       cat "zsh/setup.zsh"
+      cat "zsh/keybinds.zsh"
       cat "zsh/prompt.zsh"
       cat "zsh/completions.zsh"
       cat "zsh/history.zsh"
