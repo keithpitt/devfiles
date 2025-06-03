c2629410322df138d7e6c5787dd6493fca4325fb
Keith Pitt <git@keithpitt.email>
2025-06-03 14:37:44 +0800

    Added bat theme and setup config

--- bat.sh
+++ bat.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env devmachine
 
+BAT_CONFIG_PATH="${BAT_CONFIG_PATH:-$HOME/.config/bat}"
+
 case "$1" in
 
   setup)
     os::install "bat"
     os::install "bat-extras"
+    ;;
+
+  configure)
+    os::linkfile "bat" "$BAT_CONFIG_PATH"
+    bat cache --build
     ;;
 
   shellenv)
