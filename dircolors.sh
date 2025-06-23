f29c3f0a6cfe3717d07b1c9625a22292d17a32fe
FIRST_NAME LAST_NAME <git@keithpitt.email>
2025-06-23 15:49:39 +0800

    Added dircolors

---
+++ dircolors.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env devmachine
+
+DIRCOLORS_CONFIG_PATH="${DIRCOLORS_CONFIG_PATH:-$HOME/.config/dircolors}"
+
+case "$1" in
+
+  install)
+    os::install coreutils
+    ;;
+
+  configure)
+    os::linkfile "dircolors" "$DIRCOLORS_CONFIG_PATH"
+    ;;
+
+  shellenv)
+    gdircolors "$DIRCOLORS_CONFIG_PATH/dircolors.jellybeans"
+    ;;
+
+  --is-installed)
+    stdlib::test::is_command gdircolors && echo yes
+    ;;
+
+esac
